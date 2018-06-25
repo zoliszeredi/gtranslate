a2b5468764fcf16288db9f57b4e0a95fef2f5143
Zoltán Szeredi <zoltan.szeredi@3pillarglobal.com>
2018-06-25 19:20:08 +0300

    Scaffold.

---
+++ scaraotschi.py
@@ -0,0 +1,21 @@
+import daemon
+import logging
+
+
+logger = logging.getLogger(__file__)
+
+
+def translate(items):
+    "Send items to node process"
+    # https://www.npmjs.com/package/google-translate-api
+    pass
+
+
+
+def main():
+    # read from queue
+    # translate items
+    # output
+    with daemon.DaemonContext():
+        pass
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name='scaraotschi',
+    version='0.1.0',
+    install_requires=['python-daemon'],
+    py_modules=['scaraotschi.py'],
+)
